0e80ac38ef9453915c8b123dc28831f5834aa3a3
awnlzw <awnlzw@126.com>
2025-04-24 11:23:38 +0800

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
+    naersk = {
+      url = "github:nix-community/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
   };
